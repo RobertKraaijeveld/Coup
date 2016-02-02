d4ffd35fd8cdef695ed0e2ed823cab3583eff2e6
RobertKraaijeveld <0890289@hr.nl>
2016-02-02 01:19:10 +0100

    Started to refactor everything after not touching it for a while.

---
+++ Coup - the Revolution 3D (discontinued)/Coup - the Revolution 3D/Assets/Hand_fillHand.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Hand_fillHand : MonoBehaviour {
+
+	void Awake()
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameObject Card1;
+		GameObject Card2;
+		//how about we give every card-class its own gameobject?
+	}
+
+	public List<Card> HandContent = new List<Card>();
+	private readonly int HandSize = 2;
+	
+	public void fillHand(Deck_base deck)
+	{
+		for (int i = 0; i < HandSize; i++)
+		{
+			//HandContent.Add(deck.DrawCard());
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
